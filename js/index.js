0157de5d9f95d8bb7e66113526133788de2f8500
adhxabre <abeldustin06@gmail.com>
2023-10-05 10:25:24 +0700

    <🗿> Day 6

---
+++ js/index.js
@@ -0,0 +1,13 @@
+let hamburgerIsOpen = false;
+const openHamburger = () => {
+  let hamburgerNavContainer = document.getElementById(
+    "hamburger-nav-container"
+  );
+  if (!hamburgerIsOpen) {
+    hamburgerNavContainer.style.display = "block";
+    hamburgerIsOpen = true;
+  } else {
+    hamburgerNavContainer.style.display = "none";
+    hamburgerIsOpen = false;
+  }
+};
